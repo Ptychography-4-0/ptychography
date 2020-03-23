f352e0fe94f141f0e2482fadd50b9ecc28bd6bd8
Simeon Ehrig <s.ehrig@hzdr.de>
2020-03-23 12:09:04 +0100

    Implement a python binding for an CUDA C++ algorithm with pybind11

---
+++ prototypes/alpaka_binding/simple_cuda/binding.cu
@@ -0,0 +1,27 @@
+#include <pybind11/pybind11.h>
+#include <string>
+
+#include "algo.hpp"
+#include "cu_algo.hpp"
+#include "cu_umem_algo.hpp"
+
+namespace py = pybind11;
+
+template <typename ALGO_CLASS_T>
+void decare_algo_binding(py::module &m, const std::string &class_name) {
+  py::class_<ALGO_CLASS_T>(m, class_name.c_str(), py::module_local())
+      .def(py::init<size_t>())
+      .def("init", &ALGO_CLASS_T::init)
+      .def("deinit", &ALGO_CLASS_T::deinit)
+      .def("get_size", &ALGO_CLASS_T::get_size)
+      .def("get_input_view", &ALGO_CLASS_T::get_input_view,
+           py::return_value_policy::reference_internal)
+      .def("get_output_view", &ALGO_CLASS_T::get_output_view,
+           py::return_value_policy::reference_internal)
+      .def("compute", &ALGO_CLASS_T::compute);
+}
+
+PYBIND11_MODULE(cuBinding, m) {
+  decare_algo_binding<CuAlgo<float, int>>(m, "CuAlgoFI");
+  decare_algo_binding<CuUmemAlgo<float, int>>(m, "CuUmemAlgoFI");
+}
