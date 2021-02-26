f8201239ccd80245e543b77fb13f06863e45c0c8
Dieter Weber <d.weber@fz-juelich.de>
2021-02-26 15:03:15 +0100

    Continue CI update
    * Purge unneeded items from test setup
    * Align with current LiberTEM
    * Add an example so that `--doctest-modules` is happy

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,23 +1,6 @@
-import libertem
-import libertem.common.slice
-import libertem.executor.dask
-import libertem.executor.inline
-import libertem.io.dataset.hdf5
-import libertem.io.dataset.mib
-import libertem.io.dataset.raw
-# import libertem.io.ingest.cli
-# import libertem.io.ingest.empad
-# import libertem.io.ingest.hdf5
-# import libertem.io.ingest.sink
-import libertem.job.masks
-import libertem.job.raw
-import libertem.web.server
-import libertem.web.cli
-import libertem.api
-import libertem.preload
-import libertem.udf
-import libertem.udf.blobfinder
-import libertem.analysis.gridmatching
+import ptychography
+import ptychography.reconstruction.ssb
+import ptychography.stitching.stitching
 
 
 def test_stuff():
