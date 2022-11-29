2d66616e51072c49d344e178d48200100568d6af
Alexander Clausen <alex@gc-web.de>
2022-11-29 10:48:34 +0100

    Use `shasum` instead of `sha256sum`
    This should be available on macos in CI

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -4,7 +4,7 @@ set -e
 
 # pin the version so we can compare sha
 curl -Os https://uploader.codecov.io/v0.1.17/linux/codecov
-echo 'ca88335829e3a5b589674a200fdd1dae8f2ef27775647bc3aef6677266a6fda2 codecov' | sha256sum -c
+echo 'ca88335829e3a5b589674a200fdd1dae8f2ef27775647bc3aef6677266a6fda2  codecov' | shasum -b -a 256 -c
 
 chmod +x codecov
 ./codecov $*
