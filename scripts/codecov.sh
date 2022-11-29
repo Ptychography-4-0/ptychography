899fcf289a11b90e4bbb95d05ecb3441a7895462
Alexander Clausen <alex@gc-web.de>
2022-11-29 11:13:30 +0100

    Try to use new uploader for windows, too

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -2,12 +2,18 @@
 
 set -eu
 
+BIN=codecov
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     URL=https://uploader.codecov.io/v0.1.17/linux/codecov
-    SHA='ca88335829e3a5b589674a200fdd1dae8f2ef27775647bc3aef6677266a6fda2'
+    SHA='ca88335829e3a5b589674a200fdd1dae8f2ef27775647bc3aef6677266a6fda2  codecov'
 elif [[ "$OSTYPE" == "darwin" ]]; then
     URL='https://uploader.codecov.io/v0.3.2/macos/codecov'
-    SHA='ccc032e70958ea3eca9bd15c7fdad5bbacc279c3bab22f227417573356569666'
+    SHA='ccc032e70958ea3eca9bd15c7fdad5bbacc279c3bab22f227417573356569666  codecov'
+elif [[ "$OSTYPE" == "msys" ]]; then
+    URL='https://uploader.codecov.io/v0.3.2/windows/codecov.exe'
+    SHA='9dce1ac2d35f550d96d46f8a1116b4f03740d61ca4508bfccdbf1ea1dfeabe4d  codecov.exe'
+    BIN=codecov.exe
 else
     echo "unknown OSTYPE: $OSTYPE";
     exit 1;
@@ -15,6 +21,6 @@ fi
 
 # pin the versions so we can compare sha
 curl -Os "$URL"
-echo "$SHA  codecov" | shasum -b -a 256 -c
-chmod +x codecov
-./codecov "$@"
+echo "$SHA" | shasum -b -a 256 -c
+chmod +x "$BIN"
+./$BIN "$@"
